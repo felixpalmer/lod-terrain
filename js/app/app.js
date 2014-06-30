045169e9277b3353b3b34a621c33b941c8b3e9b5
Felix Palmer <felixpalmer@gmail.com>
2014-06-30 10:01:12 +0200

    Control mouse with height

--- js/app/app.js
+++ js/app/app.js
@@ -1,7 +1,8 @@
-define( ["three", "camera", "controls", "geometry", "material", "noise", "renderer", "scene", "terrain"],
-function ( THREE, camera, controls, geometry, material, noise, renderer, scene, Terrain ) {
+define( ["three", "camera", "container", "controls", "geometry", "material", "noise", "renderer", "scene", "terrain"],
+function ( THREE, camera, container, controls, geometry, material, noise, renderer, scene, Terrain ) {
   var app = {
     clock: new THREE.Clock(),
+    mouse: { x: 0, y: 0 },
     init: function () {
       // Terrain( heightdata, worldWidth, levels of detail, tile resolution )
       app.terrain = new Terrain( noise, 1024, 4, 64 );
@@ -14,24 +15,35 @@ function ( THREE, camera, controls, geometry, material, noise, renderer, scene, 
       
       /// Add sky2
       var sky2 = new THREE.Mesh( geometry.sky2, material.atmosphere );
-      sky2.position.z = -2500;
+      sky2.position.z = -1000;
       scene.add( sky2 );
+
+      // Mouse input
+      container.addEventListener( 'mousemove', function( e ) {
+        app.mouse = {
+          x: e.clientX - container.offsetWidth / 2,
+          y: container.offsetHeight - e.clientY
+        };
+      } );
     },
- //   height: function() {
+    height: function() {
+      return Math.min( app.mouse.y / 2 + 20, 500 );
  //     var i = Math.floor( camera.position.x % 1024 );
  //     var j = 1023 - Math.floor( camera.position.y % 1024 );
  //     //var h = 1024 * noise.image.data[ 13 ];
  //     var h = 1024 * noise.image.data[i + 1024 * j] / 255;
  //     return h * h / 2000 + 20;
- //   },
+    },
+    center: new THREE.Vector3( 25, 25, 0 ),
     animate: function () {
       window.requestAnimationFrame( app.animate );
       //controls.update( app.clock.getDelta() );
       var time = 0.5 * app.clock.getElapsedTime();
       camera.position.x = 450 * Math.cos( time / 3 ) + 200;
       camera.position.y = 250 * Math.sin( time / 4 ) + 200;
-//      camera.position.z += 0.03 * ( app.height() - camera.position.z );
-      camera.lookAt( new THREE.Vector3( 25, 25, 0 ) );
+      camera.position.z += 0.02 * ( app.height() - camera.position.z );
+      //camera.position.z = 30 + 260 * Math.pow( Math.sin( time ), 4 );
+      camera.lookAt( app.center );
       app.terrain.offset.x = camera.position.x;
       app.terrain.offset.y = camera.position.y;
       renderer.render( scene, camera );
